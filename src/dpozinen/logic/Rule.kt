d887d4a66a296bd96c616f9ed28b07fb1bc97ceb
dpozinen <dpozinen@gmail.com>
2019-12-22 20:57:36 +0200

    turning point to rethink

--- src/dpozinen/logic/Rule.kt
+++ src/dpozinen/logic/Rule.kt
@@ -1,22 +1,30 @@
 package dpozinen.logic
 
+import java.lang.IllegalArgumentException
+
 /**
  * @author dpozinen
  */
 
-class Rule {
+class Rule(val facts: List<Fact>, val results: List<Fact>) {
 
 	companion object {
 		fun parse(line: String): Rule {
+			if (!line.contains("=>")) throw IllegalArgumentException("No <implies> or <only if> found")
+
+			val facts: List<Fact> = emptyList()
+			val results: List<Fact> = emptyList()
 			val cleanRule = line.replace(Regex("\\s+"), " ").replace("! ", "")
+
 			cleanRule.split(" ").forEach { s ->
-				if (Symbol.isSymbol(s)) {
-
+				val symbol = when {
+					Symbol.isSymbol(s) -> Symbol.createFrom(s)
+					else -> Symbol.IS
 				}
+
+
 			}
-			return Rule()
+			return Rule(facts, results)
 		}
-
-
 	}
 }
