1bb02e2af6c000804681c2cd59816709bcca44cb
dpozinen <dpozinen@gmail.com>
2019-12-22 19:59:11 +0200

    re indent

--- src/dpozinen/logic/Symbol.kt
+++ src/dpozinen/logic/Symbol.kt
@@ -3,10 +3,14 @@ package dpozinen.logic
 /**
  * @author dpozinen
  */
-enum class Symbol(val symbol: String) {
-    NOT("!"),
-    AND("+"),
-    OR("|"),
-    IMPLIES("=>"),
-    ONLYIF("<=>")
+enum class Symbol(private val symbol: String) {
+	NOT("!"),
+	AND("+"),
+	OR("|"),
+	IMPLIES("=>"),
+	ONLYIF("<=>");
+
+	companion object {
+		fun isSymbol(s: String): Boolean = values().any { symbol -> symbol.symbol == s }
+	}
 }
