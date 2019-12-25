8e66ab0676b2ad65788e9dab0b79f63c91e726b8
dpozinen <dpozinen@gmail.com>
2019-12-25 22:54:59 +0200

    add flag parsing and two more falgs

--- src/dpozinen/io/Input.kt
+++ src/dpozinen/io/Input.kt
@@ -4,9 +4,20 @@ import dpozinen.logic.leaves.Leaf
 
 object Input {
 
-	var leaves: List<Leaf> = emptyList()
-	var truths: List<Leaf> = emptyList()
-	var queries: List<Leaf> = emptyList()
+	var leaves: MutableList<Leaf> = mutableListOf()
+	var truths: MutableList<Leaf> = mutableListOf()
+	var queries: MutableList<Leaf> = mutableListOf()
+
+	var fullNames: Boolean = false
 	var verbose: Boolean = false
+	var rulesAsQueries: Boolean = false
+
+//	fun saveLeaf(leaf: Leaf) = leaves.add(leaf)
+//	fun saveTruth(leaf: Leaf) = truths.add(leaf)
+//	fun saveQuery(leaf: Leaf) = queries.add(leaf)
+// 	TODO("figure out best way to handle openness of Input.class")
+//	fun enableVerbose() { verbose = true }
+//	fun enableFullNames() { fullNames = true }
+//	fun enableRulesAsQueries() { rulesAsQueries = true }
 
 }
--- src/dpozinen/io/Reader.kt
+++ src/dpozinen/io/Reader.kt
@@ -1,5 +1,7 @@
 package dpozinen.io
 
+import dpozinen.logic.leaves.Fact
+import dpozinen.logic.leaves.Leaf
 import java.io.File
 
 class Reader(private val args: Array<String>) {
@@ -7,12 +9,14 @@ class Reader(private val args: Array<String>) {
 	private val fileName: String = args[0]
 
 	fun fillInput() {
-		fillFromFile()
 		fillFlags()
+		fillFromFile()
 	}
 
 	private fun fillFlags() {
-		if (args.contains("-v")) Input.verbose = true;
+		if (args.contains("-v")) Input.verbose = true
+		if (args.contains("-raq")) Input.rulesAsQueries = true
+		if (args.contains("-fn")) Input.fullNames = true
 	}
 
 	private fun fillFromFile() {
@@ -25,16 +29,24 @@ class Reader(private val args: Array<String>) {
 			}
 	}
 
+	private fun fillQueries(line: String) {
+		fillWithLeafs(line.removePrefix("?"), Input.queries)
+	}
+
+	private fun fillTruths(line: String) {
+		fillWithLeafs(line.removePrefix("="), Input.truths)
+	}
+
 	private fun addRule(line: String) {
 		TODO()
 	}
 
-	private fun fillQueries(line: String) {
-		TODO()
+	private fun fillWithLeafs(line: String, target: MutableList<Leaf>) {
+		if (Input.rulesAsQueries || Input.fullNames)
+			line.split(",")
+				.forEach { target.add(Fact(it)) }
+		else
+			line.map { it.toString() }
+				.forEach { target.add(Fact(it)) }
 	}
-
-	private fun fillTruths(line: String) {
-		TODO()
-	}
-
 }
