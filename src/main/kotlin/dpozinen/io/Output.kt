58286942e062ce9d7197993baee60010ae12ee89
dpozinen <dpozinen@gmail.com>
2019-12-29 18:30:58 +0200

    add normal toString()

--- src/main/kotlin/dpozinen/io/Output.kt
+++ src/main/kotlin/dpozinen/io/Output.kt
@@ -2,12 +2,16 @@ package dpozinen.io
 
 import com.google.common.base.Stopwatch
 import dpozinen.logic.leaves.Leaf
+import java.util.concurrent.TimeUnit
 
 @Suppress("UnstableApiUsage")
 class Output(
-	private val statements: List<String>,
-	private val stopwatch: Stopwatch,
-	val target: Leaf
+		private val statements: List<String>,
+		private val stopwatch: Stopwatch,
+		private val target: Leaf
+) {
 
-
-)
+	override fun toString(): String {
+		return String.format("Solving %s took %s seconds:%n%s%n", target, stopwatch.elapsed(TimeUnit.SECONDS), statements.joinToString("\n"))
+	}
+}
