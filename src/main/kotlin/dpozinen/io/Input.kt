f144025730a44342881d76bffef90c1d547e77bc
dpozinen <dpozinen@gmail.com>
2020-02-02 21:59:54 +0200

    make verbose the default mode

--- src/main/kotlin/dpozinen/io/Input.kt
+++ src/main/kotlin/dpozinen/io/Input.kt
@@ -14,7 +14,7 @@ class Input {
 	var queries = mutableListOf<Leaf>()
 
 	var fullNames: Boolean = false
-	var verbose: Boolean = false
+	var verbose: Boolean = true
 	var rulesAsQueries: Boolean = false
 
 	override fun toString(): String = fullInput
